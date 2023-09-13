0b28bd2f13320c2160d2c2079d9bc43ae8f9fae2
Thasyo Peres <thasyotp2002@gmail.com>
2023-09-13 09:21:24 -0300

    add user register authentication

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,6 +1,7 @@
-//react
-import React from 'react'
+//react/hooks/router dom
+import React, { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom';
+import { useAuthentication } from '../../hooks/useAuthetication';
 
 //styles
 import styles from '../Register/Register.module.css'
@@ -8,12 +9,54 @@ import styles from '../Register/Register.module.css'
 //assets
 import bgRegister from '../../assets/bg-register-and-login.jpg'
 import { IoLogIn } from "react-icons/io5";
-import { FcGoogle } from "react-icons/fc";
 
 //components
 import Search from '../../components/Search/Search'
 
 const Register = () => {
+
+    //states do form
+    const [displayName, setDisplayName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [error, setError] = useState('')
+
+    const {createUser, error: authError, loading} = useAuthentication()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        setError('')
+
+        const user = {
+            displayName,
+            email,
+            password
+        }
+
+        if(password !== confirmPassword){
+            setError('As senhas precisam ser iguais!')
+            return
+        } 
+
+        const response = await createUser(user)
+
+        console.log(response)
+
+        setDisplayName("")
+        setEmail("")
+        setPassword("")
+        setConfirmPassword("")
+
+    }
+
+    useEffect(() => {
+        if(authError){
+            setError(authError)
+        }
+    }, [authError])
+
   return (
     <div className={styles.register}>
         <Search/>
@@ -23,32 +66,32 @@ const Register = () => {
                 <div>
                     <p>Crie uma conta e aproveite os serviços da Saboreando Receitas. Monte uma lista de receitas favoritas, envie comentários, participe da nossa Comunidade Chefzando e interaja com todos os usuários, divulgando e postando receitas, visitando receitas dos outros usuários e muito mais!</p>
                 </div>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <label>
                         <span>Nome</span>
-                        <input type="text" name='displayName' required placeholder='Digite o seu nome...'/>
+                        <input type="text" name='displayName' required placeholder='Digite o seu nome...' value={displayName} onChange={(e) => setDisplayName(e.target.value)}/>
                     </label>
 
                     <label>
                         <span>Email</span>
-                        <input type="email" name='email' required placeholder='Digite o seu email...'/>
+                        <input type="email" name='email' required placeholder='Digite o seu email...' value={email} onChange={(e) => setEmail(e.target.value)}/>
                     </label>
 
                     <label>
                         <span>Senha</span>
-                        <input type="password" name='password' required placeholder='Insira a sua senha...'/>
+                        <input type="password" name='password' required placeholder='Insira a sua senha...' value={password} onChange={(e) => setPassword(e.target.value)}/>
                     </label>
 
                     <label className={styles.lastLabel}>
                         <span>Confirmação de Senha</span>
-                        <input type="password" name='confirmPassword' required placeholder='Confirme a sua senha...'/>
+                        <input type="password" name='confirmPassword' required placeholder='Confirme a sua senha...' value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
+                        {error && <p className='error'>{error}</p>}
                     </label>
 
-                    <button className={styles.btnRegister}>Cadastrar</button>
+                    {!loading && <button className={styles.btnRegister}>Cadastrar</button>}
+                    {loading && <button className={styles.btnRegister} disabled>Aguarde...</button>}
                     
                 </form>
-
-                <button className={styles.btnRegisterGoogle}> <FcGoogle className={styles.FcGoogle}/> Cadastrar com o Google</button>
 
                 <span className={styles.existLogin}>
                     <p>Já tem uma conta? </p>
